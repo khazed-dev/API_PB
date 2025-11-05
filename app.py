05e1c907b6c51623c31f3e16f58b6b0462169c0e
khazed-dev <khatranudn@gmail.com>
2025-11-05 18:45:43 +0700

    add cors

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 import requests
 import time
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # =============================
 # Cấu hình cơ bản
